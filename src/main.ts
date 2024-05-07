6c70d3b230413bc82dec289bf2f23547540208fa
luigui044 <56895934+luigui044@users.noreply.github.com>
2024-05-06 22:57:51 -0600

    parametros de produccion

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions: CorsOptions = {
-    origin: ['http://localhost:5173'], // Cambia esto por el origen de tu aplicación Vue
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
